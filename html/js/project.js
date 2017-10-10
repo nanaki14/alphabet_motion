4952e91f9e6d110d67389293bb57f62a80d8fbeb
chiba <masatochiba0122@gmail.com>
2017-10-10 20:06:03 +0900

    ちょこっと変更

--- html/js/project.js
+++ html/js/project.js
@@ -9,33 +9,33 @@ $(function () {
 
 
     //jsonファイル指定
-    var path2 = new Array();
-    path2[65] = "./json/data_A.json"
-    path2[66] = "./json/data_B.json"
-    path2[67] = "./json/data_C.json"
-    path2[68] = "./json/data_D.json"
-    path2[69] = "./json/data_E.json"
-    path2[70] = "./json/data_F.json"
-    path2[71] = "./json/data_G.json"
-    path2[72] = "./json/data_H.json"
-    path2[73] = "./json/data_I.json"
-    path2[74] = "./json/data_J.json"
-    path2[75] = "./json/data_K.json"
-    path2[76] = "./json/data_L.json"
-    path2[77] = "./json/data_M.json"
-    path2[78] = "./json/data_N.json"
-    path2[79] = "./json/data_O.json"
-    path2[80] = "./json/data_P.json"
-    path2[81] = "./json/data_Q.json"
-    path2[82] = "./json/data_R.json"
-    path2[83] = "./json/data_S.json"
-    path2[84] = "./json/data_T.json"
-    path2[85] = "./json/data_U.json"
-    path2[86] = "./json/data_V.json"
-    path2[87] = "./json/data_W.json"
-    path2[88] = "./json/data_X.json"
-    path2[89] = "./json/data_Y.json"
-    path2[90] = "./json/data_Z.json"
+    var path = new Array();
+    path[65] = "./json/data_A.json"
+    path[66] = "./json/data_B.json"
+    path[67] = "./json/data_C.json"
+    path[68] = "./json/data_D.json"
+    path[69] = "./json/data_E.json"
+    path[70] = "./json/data_F.json"
+    path[71] = "./json/data_G.json"
+    path[72] = "./json/data_H.json"
+    path[73] = "./json/data_I.json"
+    path[74] = "./json/data_J.json"
+    path[75] = "./json/data_K.json"
+    path[76] = "./json/data_L.json"
+    path[77] = "./json/data_M.json"
+    path[78] = "./json/data_N.json"
+    path[79] = "./json/data_O.json"
+    path[80] = "./json/data_P.json"
+    path[81] = "./json/data_Q.json"
+    path[82] = "./json/data_R.json"
+    path[83] = "./json/data_S.json"
+    path[84] = "./json/data_T.json"
+    path[85] = "./json/data_U.json"
+    path[86] = "./json/data_V.json"
+    path[87] = "./json/data_W.json"
+    path[88] = "./json/data_X.json"
+    path[89] = "./json/data_Y.json"
+    path[90] = "./json/data_Z.json"
 
 
 
@@ -50,7 +50,7 @@ $(function () {
       loop: false,
       prerender: true,
       autoplay: false,
-      path: path2[e.keyCode]
+      path: path[e.keyCode]
     };
 
     var anim = bodymovin.loadAnimation(animData);
